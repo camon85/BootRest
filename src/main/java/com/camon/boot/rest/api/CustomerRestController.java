bf9d88bc9ee5e8c9ac2030634c1346adb7eada94
Jooyong Sung <camon85@gmail.com>
2015-11-11 21:31:58 +0900

    CRUD 구현

--- src/main/java/com/camon/boot/rest/api/CustomerRestController.java
+++ src/main/java/com/camon/boot/rest/api/CustomerRestController.java
@@ -3,13 +3,12 @@ package com.camon.boot.rest.api;
 import com.camon.boot.rest.domain.Customer;
 import com.camon.boot.rest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 /**
  * Created by jooyong on 2015-11-11.
@@ -26,9 +25,27 @@ public class CustomerRestController {
         return customers;
     }
 
-    @RequestMapping(value ="{id}", method = GET)
+    @RequestMapping(value = "{id}", method = GET)
     Customer getCustomer(@PathVariable Integer id) {
         Customer customer = service.findOne(id);
         return customer;
+    }
+
+    @RequestMapping(method = POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    Customer postCustomers(@RequestBody Customer customer) {
+        return service.create(customer);
+    }
+
+    @RequestMapping(value = "{id}", method = PUT)
+    Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+        customer.setId(id);
+        return service.update(customer);
+    }
+
+    @RequestMapping(value = "{id}", method = DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteCustomer(@PathVariable Integer id) {
+        service.delete(id);
     }
 }

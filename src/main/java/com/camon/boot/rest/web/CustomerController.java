a38d6dd078de0a52be483dc48bd7b6e2f07989b2
Jooyong Sung <camon85@gmail.com>
2015-11-12 12:01:55 +0900

    고객 생성

--- src/main/java/com/camon/boot/rest/web/CustomerController.java
+++ src/main/java/com/camon/boot/rest/web/CustomerController.java
@@ -2,9 +2,13 @@ package com.camon.boot.rest.web;
 
 import com.camon.boot.rest.domain.Customer;
 import com.camon.boot.rest.service.CustomerService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +29,22 @@ public class CustomerController {
         List<Customer> customers = customerService.findAll();
         model.addAttribute("customers", customers);
         return "customers/list";
+    }
+
+    @ModelAttribute
+    CustomerForm setUpForm() {
+        return new CustomerForm();
+    }
+
+    @RequestMapping(value = "create", method = POST)
+    String create(@Validated CustomerForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return list(model);
+        }
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(form, customer);
+        customerService.create(customer);
+        return "redirect:/customers";
     }
 
 }
--- src/main/java/com/camon/boot/rest/web/CustomerForm.java
+++ src/main/java/com/camon/boot/rest/web/CustomerForm.java
@@ -1,7 +1,20 @@
 package com.camon.boot.rest.web;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * Created by jooyong on 2015-11-12.
  */
+@Data
 public class CustomerForm {
+    @NotNull
+    @Size(min = 1, max = 127)
+    private String firstName;
+
+    @NotNull
+    @Size(min =1, max = 127)
+    private String lastName;
 }
